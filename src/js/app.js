024e6d60588aaf3d80e10f863d4f4b15cf4248af
fomedes <omedesf@gmail.com>
2022-04-15 21:37:55 +0100

    update

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ var IronMan = [
     "Spider-Man: Homecoming",
     "Los Vengadores: Infinity War",
     "Los Vengadores: End Game",
-    "Spider-Man: Lejos de casa",
+    "Spider-Man: Lejos de casa"
 ]
 
 var CapitanAmerica = [
@@ -25,6 +25,54 @@ var CapitanAmerica = [
     "Capitana Marvel",
     "Los Vengadores: End Game",
     "Spider-Man: Lejos de casa",
+]
+
+var BlackWidow = [
+    "Iron Man 2",
+    "Los Vengadores",
+    "Captain America: The Winter Soldier",
+    "Los Vengadores: La era de Ultrón",
+    "Capitán América: Guerra Civil",
+    "Spider-Man: Homecoming",
+    "Thor: Ragnarok",
+    "Los Vengadores: Infinity War",
+    "Captain Marvel",
+    "Los Vengadores: End Game",
+    "Black Widow",
+]
+
+var BlackPanther = [
+    "Capitán América: Guerra Civil",
+    "Spider-Man: Homecoming",
+    "Black Panther",
+    "Los Vengadores: Infinity War",
+    "Los Vengadores: End Game",
+    "Black Widow",
+]
+
+var Thor = [
+    "Thor",
+    "Capitán América: El primer vengador",
+    "Los Vengadores",
+    "Iron Man 3",
+    "Thor: The Dark World",
+    "Los Vengadores: La era de Ultrón",
+    "Doctor Strange",
+    "Thor: Ragnarok",
+    "Los Vengadores: Infinity War",
+    "Los Vengadores: End Game",
+]
+
+var Hulk = [
+    "El increíble Hulk",
+    "Los Vengadores",
+    "Iron Man 3",
+    "Los Vengadores: La era de Ultrón",
+    "Thor: Ragnarok",
+    "Los Vengadores: Infinity War",
+    "Captain Marvel",
+    "Los Vengadores: End Game",
+    "Shang-Chi y la Leyenda de los diez Anillos"
 ]
 
 
